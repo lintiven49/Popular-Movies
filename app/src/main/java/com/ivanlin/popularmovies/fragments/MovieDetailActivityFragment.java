944fcf7ba94a07b33903af03d1da5ce37d4baee9
ivan <lintiven49@gmail.com>
2015-07-25 00:56:08 +0800

    detail activity

---
+++ app/src/main/java/com/ivanlin/popularmovies/fragments/MovieDetailActivityFragment.java
@@ -0,0 +1,24 @@
+package com.ivanlin.popularmovies.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ivanlin.popularmovies.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MovieDetailActivityFragment extends Fragment {
+
+    public MovieDetailActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_movie_detail, container, false);
+    }
+}
